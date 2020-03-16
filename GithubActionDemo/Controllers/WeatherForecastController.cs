d94ac7cb476dfa44a699b2772d7e20fc0101d325
Al Abrar <firebold42@gmail.com>
2020-03-16 20:33:28 +0600

    clear comment

--- GithubActionDemo/Controllers/WeatherForecastController.cs
+++ GithubActionDemo/Controllers/WeatherForecastController.cs
@@ -10,8 +10,7 @@ namespace GithubActionDemo.Controllers
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
-    {
-        //array of weather typea
+    {        
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
